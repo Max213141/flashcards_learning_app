1cdf86143cad4c3bf4b51c01a26ab1d764d96af8
Kuptsov Maksim <maksim.kuptsov@idp.by>
2026-02-02 10:42:36 +0300

    Topic screen uncomment some content

--- lib/screens/topic_screen/topic_screen.dart
+++ lib/screens/topic_screen/topic_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flashcards_learning_app/common_widgets/custom_action_button.dart';
 import 'package:flashcards_learning_app/design/colors.dart';
 import 'package:flashcards_learning_app/entities/word.dart';
 import 'package:flashcards_learning_app/screens/topic_screen/widgets/circle_custom_action_button.dart';
@@ -55,80 +56,80 @@ class _TopicScreenState extends State<TopicScreen> {
           }
           return Column(
             children: [
-              // DecoratedBox(
-              //   decoration: BoxDecoration(
-              //     color: AppConst.primary,
-              //     borderRadius: BorderRadius.only(
-              //       bottomLeft: Radius.circular(35),
-              //       bottomRight: Radius.circular(35),
-              //     ),
-              //   ),
-              //   child: Padding(
-              //     padding: const EdgeInsets.all(30.0),
-              //     child: DecoratedBox(
-              //       decoration: BoxDecoration(
-              //         color: AppConst.transparent,
-              //         borderRadius: BorderRadius.circular(35),
-              //       ),
-              //       child: Padding(
-              //         padding: const EdgeInsets.symmetric(
-              //           horizontal: 28.0,
-              //           vertical: 10,
-              //         ),
-              //         child: Row(
-              //           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              //           children: [
-              //             Column(
-              //               crossAxisAlignment: CrossAxisAlignment.start,
-              //               children: [
-              //                 Text('Глаголы действия', style: AppConst.h2),
-              //                 Text('140 слов', style: AppConst.additionalText),
-              //               ],
-              //             ),
-              //             SizedBox(
-              //               width: 50,
-              //               height: 50,
-              //               child: DecoratedBox(
-              //                 decoration: BoxDecoration(
-              //                   shape: BoxShape.circle,
-              //                   color: Color(0xffffffff),
-              //                 ),
-              //                 child: Padding(
-              //                   padding: const EdgeInsets.all(7.0),
-              //                   child: Stack(
-              //                     alignment: AlignmentGeometry.center,
-              //                     children: [
-              //                       CircularProgressIndicator(
-              //                         value: 0.5,
-              //                         strokeWidth: 4,
-              //                         strokeCap: StrokeCap.round,
+              DecoratedBox(
+                decoration: BoxDecoration(
+                  color: AppConst.primary,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(35),
+                    bottomRight: Radius.circular(35),
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(30.0),
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(
+                      color: AppConst.transparent,
+                      borderRadius: BorderRadius.circular(35),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 28.0,
+                        vertical: 10,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('Глаголы действия', style: AppConst.h2),
+                              Text('140 слов', style: AppConst.additionalText),
+                            ],
+                          ),
+                          SizedBox(
+                            width: 50,
+                            height: 50,
+                            child: DecoratedBox(
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Color(0xffffffff),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(7.0),
+                                child: Stack(
+                                  alignment: AlignmentGeometry.center,
+                                  children: [
+                                    CircularProgressIndicator(
+                                      value: 0.5,
+                                      strokeWidth: 4,
+                                      strokeCap: StrokeCap.round,
 
-              //                         color: AppConst.lavender,
-              //                       ),
-              //                       Text('50%', style: AppConst.additionalText),
-              //                     ],
-              //                   ),
-              //                 ),
-              //               ),
-              //             ),
-              //             Text('Общий \nпрогресс', style: AppConst.text),
-              //           ],
-              //         ),
-              //       ),
-              //     ),
-              //   ),
-              // ),
+                                      color: AppConst.lavender,
+                                    ),
+                                    Text('50%', style: AppConst.additionalText),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                          Text('Общий \nпрогресс', style: AppConst.text),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
 
-              // Padding(
-              //   padding: const EdgeInsets.only(top: 20.0, bottom: 40),
-              //   child: Row(
-              //     mainAxisAlignment: MainAxisAlignment.spaceAround,
-              //     children: [
-              //       CustomActionButton(buttonText: 'Практика', width: 145),
-              //       CustomActionButton(buttonText: 'Экзамен', width: 145),
-              //     ],
-              //   ),
-              // ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20.0, bottom: 40),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    CustomActionButton(buttonText: 'Практика', width: 145),
+                    CustomActionButton(buttonText: 'Экзамен', width: 145),
+                  ],
+                ),
+              ),
               Expanded(
                 child: Padding(
                   padding: EdgeInsets.only(
